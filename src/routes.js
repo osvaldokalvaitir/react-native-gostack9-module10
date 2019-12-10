5e69297084896942ba4ae34bd481cceeffee4642
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-12-10 12:33:08 -0300

    Load de agendamentos

--- src/routes.js
+++ src/routes.js
@@ -58,6 +58,7 @@ export default (signedIn = false) =>
             Profile,
           },
           {
+            resetOnBlur: true,
             tabBarOptions: {
               keyboardHidesTabBar: true,
               activeTintColor: '#FFF',
